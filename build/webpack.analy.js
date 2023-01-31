4c4378dca30d6ec4745072e4f2c7e4b94ee24004
Cai Wei <caiwei@datagrand.com>
2023-01-31 10:05:42 +0800

    feat(*): 添加耗时分析

---
+++ build/webpack.analy.js
@@ -0,0 +1,7 @@
+// 耗时分析
+const prodConfig = require("./webpack.prod.js"); // 打包配置
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin"); // 打包速度分析插件
+const smp = new SpeedMeasurePlugin(); // 实例化
+const { merge } = require("webpack-merge"); // 合并配置方法
+
+module.exports = merge(baseConfig, {});
